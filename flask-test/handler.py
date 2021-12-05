0149111f27b20781726d39e5386274509e549178
lxb007981 <lxb007981@hotmail.com>
2021-12-05 00:59:00 +0000

    bug fix; add .dockerignore

--- flask-test/handler.py
+++ flask-test/handler.py
@@ -1,40 +1,43 @@
 import torch
 from transformers import BertTokenizer
-from model import Bert_Model
-from model import NN
+from function.model import Bert_Model
+from function.model import NN
+
+# ###################################################################
+path_prefix = '/home/app/function/'
+# path_prefix = '/home/ubuntu/openfaas-flask-torch/flask-test/'
+PATH_TO_STATE_DICT = path_prefix + 'epoch1.pth'
+TOKENIZER_PRETRAIN_PATH = path_prefix + 'bert_tokenizer_pretrained/'
+BERT_PRETRAIN_PATH = path_prefix + 'bert_pretrained/'
 
 # ###################################################################
 
-PATH_TO_STATE_DICT = 'epoch1.pth'
-TOKENIZER_PRETRAIN_PATH = 'bert_tokenizer_pretrained/'
-BERT_PRETRAIN_PATH = 'bert_pretrained/'
-
-# ###################################################################
-'''
 mode = 'bert'
 device = torch.device('cpu')
 
 tokenizer = BertTokenizer.from_pretrained(TOKENIZER_PRETRAIN_PATH)
 encoder_model = Bert_Model(device=device, mode=mode, pretrain_path=BERT_PRETRAIN_PATH)
 model = NN(encoder_model, output_dimensions=2).to(device)
-'''
-#model.eval()
-#state_dict = torch.load(PATH_TO_STATE_DICT, map_location=device)
-#model.load_state_dict(state_dict)
 
-def handle(event, context):
+model.eval()
+state_dict = torch.load(PATH_TO_STATE_DICT, map_location=device)
+model.load_state_dict(state_dict)
+
+def handle(event, _):
     test_input = event.body
     
-    #input = [tokenizer.encode_plus(test_input, max_length=512, add_special_tokens=True, return_token_type_ids=True,
-    #                       return_attention_mask=True, padding='max_length', return_tensors='pt').to(device)]
-    #decision = torch.argmax(model(input), dim=1).item()
-    decision = 1
+    input = [tokenizer.encode_plus(test_input, max_length=512, add_special_tokens=True, return_token_type_ids=True,
+                           return_attention_mask=True, padding='max_length', return_tensors='pt').to(device)]
+    decision = torch.argmax(model(input), dim=1).item()
     return {
         "statusCode": 200,
-        "body": test_input,
+        "body": decision,
         'headers': {
             'Access-Control-Allow-Headers': 'Content-Type',
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         }
     }
+
+#if __name__ == '__main__':
+#    handle(1, 1)
